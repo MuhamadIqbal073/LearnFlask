5922e32684fdb345ce9edfe3a508ae0f7a29ed3e
MuhamadIqbal073 <krenzman100@gmail.com>
2024-07-08 16:30:28 +0700

    lEARN BACKEND

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello'
+@app.route('/<id>/')
+def hello_world(id):  # put a
+    print(id)# pplication's code here
+    return render_template('main.html')
+
+@app.route('/about/<about>')
+def new(about):
+    print(about)
+    return render_template('coba.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
